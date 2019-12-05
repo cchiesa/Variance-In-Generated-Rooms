185f8ebf66ebf853048c2aa8cf6490752b22a974
joesancle <38509609+joesancle@users.noreply.github.com>
2019-12-04 23:18:33 -0500

    Unique word global list V1.0

--- RoomCompare.py
+++ RoomCompare.py
@@ -1,6 +1,7 @@
 import os
 allObjectsByRoomType = [] #list of list of objects by room type
 allRoomsByRoomType = [] #list of list of rooms by room type
+roomTypeIndex = [] #keep track of what file is associated with each index
 for filename in os.listdir("FisherRooms"): #for each set of rooms in FisherRooms
     print(filename)
     file = open("FisherRooms/" + filename,"r") #open up set of rooms
@@ -8,6 +9,8 @@ for filename in os.listdir("FisherRooms"): #for each set of rooms in FisherRooms
     objectsLine = objectsLine[0 : len(objectsLine)-1] #remove '\n' at end of string
     objects = objectsLine.split(',') #seperate objects by comma
     
+    roomTypeIndex.append(filename) #add room type to roomTypeIndex
+
     for i in range(0, len(objects)):
         objects[i] = objects[i].replace('"',"") #remove double qoutes from objects
     allObjectsByRoomType.append(objects) #add objects for specific room type to list, needed for global list later on
@@ -28,11 +31,22 @@ for filename in os.listdir("FisherRooms"): #for each set of rooms in FisherRooms
         #print(len(objects))
     print("Amount of rooms in " + filename + ": ")
     print(len(rooms))
-    allRoomsByRoomType.append(rooms) #add list of rooms to overall list of rooms, needed for later
+    allRoomsByRoomType.append(rooms) #add list of rooms to overall list of rooms, needed for global list later on
     file.close()
-    #break #DELETE only for testing, only does first file
 
+#at this point all text files have been read
 print("Amount of Room Types: ")
 print(len(allObjectsByRoomType))
 
+#now create list of all objects from every room type, no overlap
+globalObjectsList = []
 
+for i in range(0, len(allObjectsByRoomType)): #go through each list of objects
+    for o in allObjectsByRoomType[i]: #for all objects in allObjectsByRoomType at index i (room type)
+        if o not in globalObjectsList: #if object not in globalObjectsList
+            globalObjectsList.append(o) #add object to globalObjectsList
+
+print("All Objects")
+print(globalObjectsList)
+print("globalObjectsList Size: ")
+print(len(globalObjectsList))
