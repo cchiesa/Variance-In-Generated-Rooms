a5e2d3f8fa94e7e9d2c25da8e957a321d04a4547
joesancle <38509609+joesancle@users.noreply.github.com>
2019-12-04 21:08:17 -0500

    Reading in objects

--- RoomCompare.py
+++ RoomCompare.py
@@ -1,5 +1,19 @@
 import os
-for filename in os.listdir("FisherRooms"):
+for filename in os.listdir("FisherRooms"): #for each set of rooms in FisherRooms
     print(filename)
+    file = open("FisherRooms/" + filename,"r") #open up set of rooms
+    objectsLine = file.readline() #read line of objects
+    max = len(objectsLine)-1
+    objectsLine = objectsLine[0 : max]
+    objects = objectsLine.split(',')
+    
+    for i in range(0, len(objects)):
+        objects[i] = objects[i].replace('"',"")
+            
+    
+    print(objects)
+
+    file.close()
+    break#DELETE only for testing
 
 
