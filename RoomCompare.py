69a3147787bf8b12fca9d92576b74e7344e95baa
cchiesa <cchiesa@students.stonehill.edu>
2019-12-04 20:20:40 -0500

    read rooms1

--- RoomCompare.py
+++ RoomCompare.py
@@ -1,14 +1,9 @@
 import os
-<<<<<<< HEAD
 
 for filename in os.listdir("FisherRooms"):
     # do your stuff
     print(filename)
 
 
-=======
-for filename in os.listdir("FisherRooms"):
-    print(filename)
->>>>>>> f3de9c0e6f0d1f50c7b976a7db7b7c222cd64e4a
 
 
