be5e86c5bb46d6f12fd9f1d3e8a269138c60e678
cchiesa <cchiesa@students.stonehill.edu>
2019-12-07 14:07:33 -0500

    inti commit b

---
+++ TestVariance.py
@@ -0,0 +1,10 @@
+# Tests Variance of All Rooms
+import pip
+import numpy as np 
+import pandas as pd 
+import os
+import matplotlib.pyplot as plt
+
+# make data frame
+df = pd.read_csv('GenRooms.csv')
+print(df)
