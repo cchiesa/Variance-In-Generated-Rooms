ad69a46b45a500c616bc4b4e4d171c67176641ad
cchiesa <cchiesa@students.stonehill.edu>
2019-12-08 22:02:14 -0500

    Update Clusters

--- ClusterRooms.py
+++ ClusterRooms.py
@@ -12,19 +12,46 @@ df = pd.read_csv('GenRooms.csv')
 roomType = df['RoomType']
 
 del df['RoomType']
-rows = df.sample(frac = .05)
-data = rows
+data = df.sample(frac = .05)
+
+
 var_names = df.columns.values
 print(data)
-km = KModes(n_clusters=4, init='Huang', n_init=5, verbose=1)
+km = KModes(n_clusters=3, init='random', n_init=2, verbose=1)
 
-clusters = km.fit_predict(data)
+
+clusters = km.fit(data)
+print(clusters)
 
 # Print the cluster centroids
-print(km.cluster_centroids_)
+#print(km.cluster_centroids_)
 
 file = open("outfile.txt","w") 
-
+print("CENTERS")
 for i in km.cluster_centroids_:
-    file.write(i)
-file.close()
+    for j in i:
+        #print j, 
+        file.write(str(j))  
+    print(" ")
+    file.write("\n")
+
+
+print('Final training cost: {}'.format(km.cost_))
+print('Training iterations: {}'.format(km.n_iter_))
+#print df.loc[354]
+indices = []
+for center in km.cluster_centroids_:
+ #   print "1"
+    for index, row in df.iterrows():
+        #print index, " "
+        if np.array_equal( np.array(center) , np.array(row) ):
+            indices.append(index)   
+            print index 
+            break
+print indices
+#print indicies[0]
+f = open('indices.txt')
+for i in indices:
+    f.write(str(i))
+    f.write("\n")
+
