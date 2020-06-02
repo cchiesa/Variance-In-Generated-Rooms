8af6fa10fdfc36bae131d97d414e2d3d4b0d4fb0
joesancle <38509609+joesancle@users.noreply.github.com>
2020-06-01 21:10:21 -0400

    generator data

--- getResults2.py
+++ getResults2.py
@@ -3,18 +3,19 @@ import sqlite3
 import numpy as np
 from scipy.cluster.hierarchy import median
 
+# get all comparions of bath-beds
 
 
-#get all comparions of bath-beds
 def getNum(answer):
     if(answer == 'extremelyDissimilar'):
-        return 3  
+        return 3
     elif(answer == 'dissimilar'):
         return 2
-    elif(answer== 'similar'):
-        return 1      
+    elif(answer == 'similar'):
+        return 1
     elif(answer == 'extremelySimilar'):
         return 0
+
 
 conn = sqlite3.connect('websiteDatabase.db')
 
@@ -25,7 +26,46 @@ for row in c.execute("SELECT  Answer from answer WHERE (firstImage LIKE '%Bathro
     print(row)
     bed_baths.append(getNum(row[0]))
 print(bed_baths)
-        
 
+# below are generator comparisons
+kermani_sceneseer = []
+print("kermani_sceneseer")
+for row in c.execute("SELECT  Answer from answer WHERE (firstImage LIKE '%Kermani%' and secondImage LIKE '%SceneSeer%') or (secondImage LIKE '%Kermani%'and firstImage LIKE'%SceneSeer%') "):
+    print(row)
+    kermani_sceneseer.append(getNum(row[0]))
+print(kermani_sceneseer)
 
+sceneseer_custom = []
+print("sceneseer_custom")
+for row in c.execute("SELECT  Answer from answer WHERE (firstImage LIKE '%SceneSeer%' and secondImage LIKE '%Custom%') or (secondImage LIKE '%SceneSeer%'and firstImage LIKE'%Custom%') "):
+    print(row)
+    sceneseer_custom.append(getNum(row[0]))
+print(sceneseer_custom)
 
+custom_kermani = []
+print("custom_kermani")
+for row in c.execute("SELECT  Answer from answer WHERE (firstImage LIKE '%Custom%' and secondImage LIKE '%Kermani%') or (secondImage LIKE '%Custom%'and firstImage LIKE'%Kermani%') "):
+    print(row)
+    custom_kermani.append(getNum(row[0]))
+print(custom_kermani)
+
+kermani_kermani = []
+print("kermani_kermani")
+for row in c.execute("SELECT  Answer from answer WHERE (firstImage LIKE '%Kermani%' and secondImage LIKE '%Kermani%') "):
+    print(row)
+    kermani_kermani.append(getNum(row[0]))
+print(kermani_kermani)
+
+sceneseer_sceneseer = []
+print("sceneseer_sceneseer")
+for row in c.execute("SELECT  Answer from answer WHERE (firstImage LIKE '%SceneSeer%' and secondImage LIKE '%SceneSeer%') "):
+    print(row)
+    bed_baths.append(getNum(row[0]))
+print(bed_baths)
+
+custom_custom = []
+print("custom_custom")
+for row in c.execute("SELECT  Answer from answer WHERE (firstImage LIKE '%Custom%' and secondImage LIKE '%Custom%') "):
+    print(row)
+    custom_custom.append(getNum(row[0]))
+print(custom_custom)
